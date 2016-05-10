7de2518ed31d870f29e0eacc371ff2139471a116
dvrico <rico774@gmail.com>
2016-05-10 10:05:19 -0400

    Add menu icon and click events to header.
    Bring back the jquery script to control the click events.

---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".menu").click(function(e){e.stopPropagation(),$("#main_nav").toggleClass("active"),$("#top_header").toggleClass("active")}),$(document).click(function(){$("#main_nav").removeClass("active"),$("#top_header").removeClass("active")})});
